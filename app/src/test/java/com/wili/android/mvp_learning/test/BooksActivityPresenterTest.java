8eb131784b183136b83457e99edc6c96a3f9720d
damwilin <damian.wilinski@gmail.com>
2018-02-23 21:00:05 +0100

    3episode

--- app/src/test/java/com/wili/android/mvp_learning/test/BooksActivityPresenterTest.java
+++ app/src/test/java/com/wili/android/mvp_learning/test/BooksActivityPresenterTest.java
@@ -1,86 +1,60 @@
 package com.wili.android.mvp_learning.test;
 
 import com.wili.android.mvp_learning.presenter.BooksActivityPresenter;
-import com.wili.android.mvp_learning.view.BooksActivityView;
-import com.wili.android.mvp_learning.repository.model.Book;
 import com.wili.android.mvp_learning.repository.BooksRepository;
+import com.wili.android.mvp_learning.repository.model.Book;
+import com.wili.android.mvp_learning.view.BooksActivityView;
 
-import junit.framework.Assert;
-
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
 
 /**
  * Created by Damian on 22.02.2018.
  */
 public class BooksActivityPresenterTest {
 
+    @Mock
+    BooksRepository booksRepository;
+
+    @Mock
+    BooksActivityView view;
+
+    BooksActivityPresenter presenter;
+    private final List<Book> MANY_BOOKS = Arrays.asList(new Book(), new Book(), new Book());
+
+    @Before
+    public void setUp() throws Exception {
+        presenter = new BooksActivityPresenter(view, booksRepository);
+    }
+
     @Test
     public void shouldPassBooksToView() {
 
-        //given
-        BooksActivityView view = new MockView();
-        BooksRepository booksRepository = new MockBooksRepository(true);
 
-        //when
-        BooksActivityPresenter presenter = new BooksActivityPresenter(view, booksRepository);
+        Mockito.when(booksRepository.getBooks()).thenReturn(MANY_BOOKS);
+
         presenter.loadBooks();
 
-        //then
-        Assert.assertEquals(true, ((MockView) view).displayBooksWithBooksCalled);
-
+        Mockito.verify(view).displayBooks(MANY_BOOKS);
     }
 
     @Test
     public void shouldHandleNoBooksFound() {
 
-        //given
-        BooksActivityView view = new MockView();
-        BooksRepository booksRepository = new MockBooksRepository(false);
+        Mockito.when(booksRepository.getBooks()).thenReturn(Collections.EMPTY_LIST);
 
-        //when
-        BooksActivityPresenter presenter = new BooksActivityPresenter(view, booksRepository);
         presenter.loadBooks();
 
-        //then
-        Assert.assertEquals(true, ((MockView) view).displayBooksWithNoBooksCalled);
-    }
-
-    private class MockView implements BooksActivityView {
-
-        boolean displayBooksWithBooksCalled;
-        boolean displayBooksWithNoBooksCalled;
-
-        @Override
-        public void displayBooks(List<Book> bookList) {
-            if (bookList.size() == 3)
-                displayBooksWithBooksCalled = true;
-        }
-
-        @Override
-        public void displayNoBooks() {
-            displayBooksWithNoBooksCalled = true;
-        }
-    }
-
-    private class MockBooksRepository implements BooksRepository {
-
-        private boolean returnSomeBook;
-
-        public MockBooksRepository(boolean returnSomeBook) {
-            this.returnSomeBook = returnSomeBook;
-        }
-
-        @Override
-        public List<Book> getBooks() {
-
-            if (returnSomeBook)
-                return Arrays.asList(new Book(), new Book(), new Book());
-            else
-                return Collections.emptyList();
-        }
+        Mockito.verify(view).displayNoBooks();
     }
 }
